b51fb53658cd8b25e4eb07e2fbc527edd7691c62
Diego Cedillo <92384848+DiegoCedilloCastillo@users.noreply.github.com>
2021-12-09 07:55:32 -0600

    Add files via upload

---
+++ AppLoteria/AppLoteria/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace AppLoteria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bntGane = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bntGane
+            // 
+            this.bntGane.Cursor = System.Windows.Forms.Cursors.Default;
+            this.bntGane.Location = new System.Drawing.Point(625, 298);
+            this.bntGane.Margin = new System.Windows.Forms.Padding(2);
+            this.bntGane.Name = "bntGane";
+            this.bntGane.Size = new System.Drawing.Size(105, 45);
+            this.bntGane.TabIndex = 0;
+            this.bntGane.Text = "Victoria!!";
+            this.bntGane.UseVisualStyleBackColor = true;
+            this.bntGane.Click += new System.EventHandler(this.bntGane_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(959, 609);
+            this.Controls.Add(this.bntGane);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bntGane;
+    }
+}
+
---
+++ AppLoteria/AppLoteria/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppLoteria
+{
+    public partial class Form1 : Form
+    {
+        const int CANTIDAD_CARTAS = 54;
+        private PictureBox[] tabla;
+        public Form1()
+        {
+            InitializeComponent();
+            tabla = new PictureBox[25];
+            inicializarTabla();
+        }
+
+        private void inicializarTabla()
+        {
+            int renglon = 0, columna = 0;
+
+            int[] cartas = new int[CANTIDAD_CARTAS];
+
+            for (int i = 0; i < cartas.Length; i++)
+            {
+                cartas[i] = i + 1;
+            }
+
+            Random rnd = new Random();
+            int a, aux;
+            for (int i = 0; i < cartas.Length; i++)
+            {
+                a = rnd.Next(cartas.Length);
+                aux = cartas[i];
+                cartas[i] = cartas[a];
+                cartas[a] = aux;
+
+            }
+
+            for (int i = 0; i < tabla.Length; i++)
+            {
+                tabla[i] = new PictureBox();
+                tabla[i].Location = new System.Drawing.Point(100 + (columna * 90), 25 + (renglon * 125));
+                tabla[i].Name = "picTabla" + i;
+                tabla[i].Size = new System.Drawing.Size(85, 110);
+                tabla[i].TabIndex = 0 + i;
+                tabla[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                tabla[i].TabStop = false;
+                tabla[i].Image = Image.FromFile(@"C:\Users\dcedi\Downloads\Loteria\Imagenes\" +(i+1)+ ".jpg");
+                this.Controls.Add(tabla[i]);
+                columna++;
+                if (columna == 5)
+                {
+                    renglon++;
+                    columna = 0;
+                }
+            }
+        }
+
+        private void bntGane_Click(object sender, EventArgs e)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Users\dcedi\Downloads\Loteria\Sonidos\1.wav");
+            player.Play();
+        }
+    }
+}
